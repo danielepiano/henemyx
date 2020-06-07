1895f7e3101d51a800a5431bdbf7df546920b853
danielepiano <61870628+danielepiano@users.noreply.github.com>
2020-06-07 17:13:12 +0200

    Update index.php

--- home/index.php
+++ home/index.php
@@ -8,13 +8,13 @@
         $con = mysqli_connect("127.0.0.1", "root", "", "my_henemyx") or die("Errore nella connessione.");
 
         // CONTROLLO SE IL nomeutente E' REALMENTE ASSOCIATO ALL'idutente
-        $query = "SELECT idutente FROM utenti WHERE nomeutente = '$nomeutente'";
+        /*$query = "SELECT idutente FROM utenti WHERE nomeutente = '$nomeutente'";
         $result = mysqli_query($con, $query);
         $result = mysqli_fetch_array($result);
         $idut = $result["idutente"];
         if($idut != $idutente)
             header("location: ../index.php?feedback=0");
-        else {
+        else {*/
             $query = "SELECT darkmode FROM utenti WHERE idutente = $idutente";
             $result = mysqli_query($con, $query);
             $result = mysqli_fetch_array($result);
